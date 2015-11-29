262b80b4a3e61e1bc83b12051e2142ed982ae000
lmangieri <leandro.mangieri02@gmail.com>
2015-11-29 10:24:44 -0300

    mudança no imagereader

--- src/main/java/algorithm/ImageReader.java
+++ src/main/java/algorithm/ImageReader.java
@@ -9,10 +9,9 @@ import javax.imageio.ImageIO;
 
 public class ImageReader {
 	public static Image getFormatedImage(String path, Double resize){
-		File f = new File(ImageReader.class.getClass()
-				.getResource(path).getPath());
 		try {
-			BufferedImage img = ImageIO.read(f);
+			BufferedImage img = ImageIO.read(ImageReader.class.getClass()
+					.getResourceAsStream(path));
 			return img.getScaledInstance((int)(img.getWidth()/resize) , (int)(img.getHeight() /resize), Image.SCALE_SMOOTH);
 		} catch (IOException e) {
 			e.printStackTrace();
